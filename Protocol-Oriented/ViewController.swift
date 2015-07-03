54fd6ec332cc634dca5928901108132ea0501718
Ryan Arana <ryan.arana@gmail.com>
2015-07-03 13:49:16 -0700

    Add empty project

---
+++ Protocol-Oriented/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Protocol-Oriented
+//
+//  Created by Ryan Arana on 7/3/15.
+//  Copyright © 2015 pdx-ios.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Protocol-OrientedUITests/Protocol_OrientedUITests.swift
@@ -0,0 +1,35 @@
+//
+//  Protocol_OrientedUITests.swift
+//  Protocol-OrientedUITests
+//
+//  Created by Ryan Arana on 7/3/15.
+//  Copyright © 2015 pdx-ios.org. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class Protocol_OrientedUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
